54d6599de83ef30e940152742c1f1ca49ad25879
Izzy Lancaster <avrisaac555@gmail.com>
2021-09-13 17:04:33 -0700

    Merged Scale component into LinearCalendar

--- src/components/LinearCalendar/index.tsx
+++ src/components/LinearCalendar/index.tsx
@@ -1,5 +1,4 @@
-import Scale, {ScaleProps} from "./Scale"
-import { differenceInCalendarDays, Interval, toDate } from "date-fns";
+import { addDays, differenceInCalendarDays, format, formatISO, Interval, toDate } from "date-fns";
 import "./LinearCalendar.css"
 import SegmentedBar from "./SegmentedBar";
 
@@ -8,15 +7,22 @@ export interface LinearCalendarProps {
     scaleTickRate?: number
 }
 
+function createTickDates(startDate: Date, endDate: Date, tickRate: number) {
+    const tickCount = Math.ceil(differenceInCalendarDays(startDate, endDate) / tickRate)
+    return Array.from({length: tickCount}, (_, i) => addDays(startDate, i * tickRate))
+}
+
+
 export default function LinearCalendar(props: LinearCalendarProps) {
     const scaleTickRate = props.scaleTickRate || 7
-    const totalTickCount = Math.ceil(differenceInCalendarDays(props.scaleInterval.end, props.scaleInterval.start) / scaleTickRate)
+    const tickDates = createTickDates(toDate(props.scaleInterval.start), toDate(props.scaleInterval.end), scaleTickRate)
     
     return (
         <div className="LinearCalendar">
-            <Scale  startingDate={toDate(props.scaleInterval.start)} 
-                    tickRate={scaleTickRate} 
-                    ticks={totalTickCount}/>
+            {tickDates.map((v, i) => <time 
+                key={i} 
+                style={{gridColumn: i + 1}} 
+                dateTime={formatISO(v, { representation: "date"})}>{format(v, "LLL d")}</time>)}
         </div>
     );
 }
