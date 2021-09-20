f077d46050a4cd14de57b3e31736a5adf942e382
Izzy Lancaster <avrisaac555@gmail.com>
2021-09-20 15:48:57 -0700

    New design

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,31 +1,4 @@
-import React from 'react'
-import LinearCalendar from '../LinearCalendar';
 import './App.css'
-
-const TestData = [
-    {
-        name: "Dunder Mifflin",
-        progression: [
-            { status: "Applied", date: new Date(2021, 6, 15) },
-            { status: "Phone Screen", date: new Date(2021, 7, 24) },
-            { status: "Technical Interview", date: new Date(2021, 9, 2) }
-        ]
-    },
-    {
-        name: "Quills and Sofas",
-        progression: [
-            { status: "Applied", date: new Date(2021, 8, 14) },
-            { status: "Phone Screen", date: new Date(2021, 8, 18) },
-            { status: "Rejected", date: new Date(2021, 8, 22) }
-        ]
-    },
-    {
-        name: "Hay's Hardware",
-        progression: [
-            { status: "Applied", date: new Date(2021, 9, 8) }
-        ]
-    }
-]
 
 function App() {
     return (
@@ -33,8 +6,25 @@ function App() {
             <header>
                 <h1>Temperance</h1>
             </header>
-            <LinearCalendar
-                scaleInterval={{start: new Date(2021, 6, 15), end: new Date(2021, 9, 8)}} />
+            <section className="ItemList">
+                <section className="Item">
+                    <h2>Dunder Mifflin</h2>
+                    <div className="row">
+                        <div className="datum-block">
+                            <h3>POSITION</h3>
+                            <span>Sales associate</span>
+                        </div>
+                        <div className="datum-block">
+                            <h3>LINK</h3>
+                            <span><a href="https://example.com">https://example.com</a></span>
+                        </div>
+                        <div className="datum-block">
+                            <h3>STATUS</h3>
+                            <span>Final Interview</span>
+                        </div>
+                    </div>
+                </section>
+            </section>
         </div>
     );
 }
