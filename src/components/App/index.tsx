68717fb53c5431eb1edbf7e72f7f0906c521ba5c
Izzy Lancaster <avrisaac555@gmail.com>
2021-09-21 18:19:47 -0700

    Revert "Moved job application into its own component"
    This reverts commit 4370dfcbff33ca913994b8aa4ce5fcffe9c0321f.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,5 @@
 import './App.css'
 
-import JobAppView from '../JobAppView';
-
 function App() {
     return (
         <div className="App">
@@ -9,7 +7,23 @@ function App() {
                 <h1>Temperance</h1>
             </header>
             <section className="ItemList">
-                <JobAppView name="Dunder Mifflin" position="Sales Associate" link="https://example.com" status="final interview" />
+                <section className="Item">
+                    <h2>Dunder Mifflin</h2>
+                    <div className="row">
+                        <div className="datum-block">
+                            <h3>POSITION</h3>
+                            <span>Sales associate</span>
+                        </div>
+                        <div className="datum-block">
+                            <h3>LINK</h3>
+                            <span><a href="https://example.com">https://example.com</a></span>
+                        </div>
+                        <div className="datum-block">
+                            <h3>STATUS</h3>
+                            <span>Final Interview</span>
+                        </div>
+                    </div>
+                </section>
             </section>
         </div>
     );
