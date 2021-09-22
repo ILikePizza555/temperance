c35b50d27a6fa9de3ada7c0b0a75ce6d9192ede0
Izzy Lancaster <avrisaac555@gmail.com>
2021-09-21 18:32:53 -0700

    Added test data to App\index.tsx

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,25 @@
 import './App.css'
 
+const ApplicationData = [
+    {
+        company_name: "Dunder Mifflin",
+        position_title: "Sales associate",
+        position_link: "https://example.com",
+        status: "Final Interview"
+    },
+    {
+        company_name: "Aperature Science",
+        position_title: "Marketing and Sales Engineer Expert",
+        status: "Practical Assessment"
+    },
+    {
+        company_name: "Black Mesa",
+        position_title: "Goverment Advertising Associate",
+        position_link: "https://blackmesa.org",
+        status: "Second Interview"
+    }
+]
+
 function App() {
     return (
         <div className="App">
